92acaed1dd88616d4298699199941799f760311e
Krish-2806 <krish.prajapati.2806@gmail.com>
2026-07-18 14:33:59 +0530

    Integrated iTunes API and updated search functionality

--- src/services/DeexerApi.js
+++
@@ -1,8 +0,0 @@
-// const BASE_URL = "https://corsproxy.io/?https://api.deezer.com";
-
-// export const searchSongs = async (query) => {
-//   const response = await fetch(`${BASE_URL}/search?q=${query}`);
-//   const data = await response.json();
-
-//   return data.data;
-// };
